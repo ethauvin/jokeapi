90d5e57fac0cc1fab991a96230c0386c2c1727f1
Erik C. Thauvin <erik@thauvin.net>
2023-06-29 11:35:43 -0700

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,15 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.46.0"
-    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
     id("java")
     id("maven-publish")
-    id("org.jetbrains.dokka") version "1.7.20"
-    id("org.jetbrains.kotlinx.kover") version "0.6.1"
-    id("org.sonarqube") version "4.0.0.2929"
+    id("org.jetbrains.dokka") version "1.8.20"
+    id("org.jetbrains.kotlinx.kover") version "0.7.2"
+    id("org.sonarqube") version "4.2.1.3168"
     id("signing")
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.22"
 }
 
 description = "Wrapper for Sv443's JokeAPI"
@@ -33,11 +33,11 @@ dependencies {
     implementation(platform(kotlin("bom")))
 
     implementation("net.thauvin.erik:urlencoder:1.3.0")
-    implementation("org.json:json:20220924")
+    implementation("org.json:json:20230618")
 
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.26.1")
 }
 
 java {
@@ -52,7 +52,7 @@ sonarqube {
         property("sonar.organization", "ethauvin-github")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.sourceEncoding", "UTF-8")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/kover/xml/report.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/kover/report.xml")
     }
 }
 
