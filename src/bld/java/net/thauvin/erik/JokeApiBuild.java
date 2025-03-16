4cc92e956f1cfb783a0a5c392ff897b567a5cfc3
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 15:37:59 -0700

    Update root pom

--- src/bld/java/net/thauvin/erik/JokeApiBuild.java
+++ src/bld/java/net/thauvin/erik/JokeApiBuild.java
@@ -167,6 +167,12 @@ public class JokeApiBuild extends Project {
         pomRoot();
     }
 
+    @Override
+    public void publishLocal() throws Exception {
+        super.publishLocal();
+        pomRoot();
+    }
+
     @BuildCommand(value = "pom-root", summary = "Generates the POM file in the root directory")
     public void pomRoot() throws FileUtilsErrorException {
         PomBuilder.generateInto(publishOperation().fromProject(this).info(), dependencies(),
