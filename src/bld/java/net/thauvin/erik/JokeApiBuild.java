4b4f778490a9ddd20ce2a946d9ac47b7aa76a069
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 17:29:06 -0700

    Version 1.0.0

--- src/bld/java/net/thauvin/erik/JokeApiBuild.java
+++ src/bld/java/net/thauvin/erik/JokeApiBuild.java
@@ -60,7 +60,7 @@ public class JokeApiBuild extends Project {
     public JokeApiBuild() {
         pkg = "net.thauvin.erik";
         name = "jokeapi";
-        version = version(1, 0, 0, "SNAPSHOT");
+        version = version(1, 0, 0);
 
         javaRelease = 11;
         downloadSources = true;
