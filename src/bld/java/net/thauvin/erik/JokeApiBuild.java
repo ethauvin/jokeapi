f351b5eb389b481e77e22d25181b4c88112a1bf5
Erik C. Thauvin <erik@thauvin.net>
2025-09-12 09:32:55 -0700

    Bump Kotlin to version 2.2.20

--- src/bld/java/net/thauvin/erik/JokeApiBuild.java
+++ src/bld/java/net/thauvin/erik/JokeApiBuild.java
@@ -72,7 +72,7 @@ public class JokeApiBuild extends Project {
 
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
-        final var kotlin = version(2, 2, 10);
+        final var kotlin = version(2, 2, 20);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
                 .include(dependency("org.json", "json", "20250517"))
