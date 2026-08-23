14486495abd23a26ffea69e365e0667e8248afaa
Erik C. Thauvin <erik@thauvin.net>
2026-08-23 16:23:04 -0700

    Bump SpotBugs Annotations to version 4.10.4

--- src/bld/java/net/thauvin/erik/JokeApiBuild.java
+++ src/bld/java/net/thauvin/erik/JokeApiBuild.java
@@ -80,7 +80,7 @@ public class JokeApiBuild extends Project {
                         version(1, 6, 0)));
         scope(provided)
                 .include(dependency("com.github.spotbugs", "spotbugs-annotations",
-                        version(4, 10, 3)));
+                        version(4, 10, 4)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-testing-helpers",
                         version(1, 1, 0, "SNAPSHOT")))
